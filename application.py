c148e6420effa29b458cb2894e3135fed68e53ac
Aman Agrawala <amanagrawala@gmail.com>
2016-03-24 01:47:55 -0400

    feat: Add server-side code to add, delete and edit items. Also add code to view item descriptions and list of items for a given category.

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 
+import json
 
 # Imports for creating login_sessions
 
@@ -22,44 +23,96 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 # JSON APIs to view Catalog information
-@app.route('/catalog.json')
-def catalogJSON():
-	catalogs = session.query(Catalog).all()
+
+# This is the JSON for the categories
+@app.route('/categoryJSON')
+def categoryJSON():
+	categories = session.query(Category).all()
+	return jsonify(Category = [category.serialize for category in categories])
+
+# This is the JSON for the items
+@app.route('/itemsJSON')
+def itemJSON():
 	items = session.query(Item).all()
-	return "serialized list of catalog and their respective items"
+	return jsonify(Item = [item.serialize for item in items])
 
 #Show latest items (homepage)
 @app.route('/')
 @app.route('/home')
 def homepage():
-	catalog = session.query(Category).all()
+	categories = session.query(Category).all()
 	latestItems = session.query(Item).order_by(desc(Item.id)).limit(10)
 	# print [i.name for i in latestItems]
-	return 'homepage'
+	return render_template('home.html', categories = categories, latestItems = latestItems)
 
+# Lists the items for a specific category
 @app.route('/category/<int:category_id>/')
 def itemList(category_id):
 	items = session.query(Item).filter_by(category_id = category_id).all()
-	return "category list"
+	return render_template('category.html', cat_id = category_id, items = items)
 
+# This will display the item, its picture and its description
 @app.route('/category/<int:category_id>/<int:item_id>/')
 def itemDescription(category_id,item_id):
 	item = session.query(Item).filter_by(id = item_id).one()
-	return "item description"
+	return render_template('itemDescription.html', item = item)
 
-@app.route('/category/<int:category_id>/<int:item_id>/new')
-def newItem(category_id,item_id):
-	return "add new item"
+# This will allow a user to create a new item.
+@app.route('/category/<int:category_id>/new', methods = ['GET', 'POST'])
+def newItem(category_id):
+	cat = session.query(Category).filter_by(id = category_id).one()
+	print request.method
+	print request.form
+	if request.method == 'POST':
+		newItem = Item(name = request.form['Title'], description = request.form['description'],
+					   picture = request.form['picture'])
+		session.add(newItem)
+		newItem.category_id = category_id
+		newItem.user_id = 1
+		session.add(newItem)
+		session.commit()
+		flash('New item added')
+		return redirect(url_for('itemList', category_id=category_id))
+	else:
+		return render_template('newItem.html', category_id = category_id)
 
-@app.route('/category/<int:category_id>/<int:item_id>/edit')
+# This will allow a user to edit an item.
+@app.route('/category/<int:category_id>/<int:item_id>/edit', methods = ['GET', 'POST'])
 def editItem(category_id,item_id):
 	item = session.query(Item).filter_by(id = item_id).one()
-	return "edit the item"
+	if request.method == 'POST':
+		cats = session.query(Category).all()
+		if request.form.get('category'):
+			for cat in cats:
+				if cat.name == request.form['category']:
+					item.category_id = cat.id
+				else:
+					flash('Invalid Category!')
+					return redirect(url_for('itemDescription', category_id = category_id, item_id = item_id))
+		if request.form.get('Title'):
+			item.name = request.form['Title']
+		if request.form.get('description'):
+			item.description = request.form['description']
+		if request.form.get('picture'):
+			item.picture = request.form['picture']
+		session.add(item)
+		session.commit()
+		flash("Item has been added")
+		return redirect(url_for('itemList', category_id = category_id))
+	else:
+		return render_template('editItem.html', item = item)
 
-@app.route('/category/<int:category_id>/<int:item_id>/delete')
+# This will allow a user to delete an item.
+@app.route('/category/<int:category_id>/<int:item_id>/delete', methods = ['GET','POST'])
 def deleteItem(category_id,item_id):
 	item = session.query(Item).filter_by(id = item_id).one()
-	return "delete the item"
+	if request.method == 'POST':
+		flash('Item has been deleted')
+		session.delete(item)
+		session.commit()
+		return redirect(url_for('itemList',category_id = category_id))
+	else:
+		return render_template('deleteItem.html', item = item)
 
 if __name__ == '__main__':
 	app.secret_key = 'secret_key'
